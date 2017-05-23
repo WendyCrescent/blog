fde18845b878579fe6a53d1f1138229f7142095d
Wendy Crescent <wendy@wendy-crescent.com>
2017-05-23 13:08:02 +0200

    removed comment section

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -35,26 +35,4 @@
   </article>
 </div>
 
-<div class="row article__comments">
-  <div class="col-md-8 col-md-offset-2">
-    <div class="well">
-      <h4>Leave a Comment:</h4>
-      <form role="form">
-        <div class="form-group">
-          <textarea class="form-control" rows="3"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-    </div>
-
-    <hr>
-    <div class="media article__comment">
-      <a class="pull-left" href="#"> <img class="media-object" src="https://placehold.it/64x64" alt=""> </a>
-      <div class="media-body">
-        <h4 class="media-heading">User Name <small>August 25, 2014 at 9:30 PM</small> </h4>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-      </div>
-    </div>
-  </div>
-</div>
 @endsection
